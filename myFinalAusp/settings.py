3d979df455439afa51caf0dbd2bad62a91342d60
AZHIK <92061198+AZHIK@users.noreply.github.com>
2024-03-14 21:57:55 +0300

    Update settings.py

--- myFinalAusp/settings.py
+++ myFinalAusp/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'myFinalAusp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myPgUsers',
+        'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'admin@1234',
-        'HOST': 'localhost',  
-        'PORT': '5432', 
+        'PASSWORD': 'jAEKfOWKOIusRbiCVJZjXZQnthAxXriT',
+        'HOST': 'monorail.proxy.rlwy.net',  
+        'PORT': '33934', 
     }
 }
 
